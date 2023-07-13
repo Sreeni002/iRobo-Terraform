e2d7249fd4d285a80ac791eceb062b9af7d03859
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-13 20:29:56 +0530

    Making multi envs

--- state.tf
+++ state.tf
@@ -1,7 +1,11 @@
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-irobo"
+#    key = "irobo/dev/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
+
 terraform {
-  backend "s3" {
-    bucket = "terraform-irobo"
-    key = "irobo/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
