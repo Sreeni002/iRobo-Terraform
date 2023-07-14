1fab05cfcca766a9dbf86b2565b93fa963fb3ff6
Sreeni.B <sreenu.reddy002@gmail.com>
2023-07-14 07:47:23 +0530

    Working with provisioner

--- servers.tf
+++ servers.tf
@@ -1,20 +1,36 @@
-
 resource "aws_instance" "instance" {
-  for_each                = var.components
-  ami                     = data.aws_ami.centos.image_id
-  instance_type           = each.value["instance_type"]
-  vpc_security_group_ids  = [ data.aws_security_group.allow-all.id ]
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = each.value["name"]
   }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = "self.private_ip"
+    }
+
+    inline = [
+      "rm-rf iRobo-shell",
+      "git clone https://github.com/Sreeni002/iRobo-shell.git",
+      "cd iRobo-shell",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
 }
 
-resource "aws_route53_record" "records" {
-  for_each  = var.components
-  zone_id   = "Z01796182PXCN3W0FPGMA"
-  name      = "${each.value["name"]}-test.sreenivasulareddydevops.online"
-  type      = "A"
-  ttl       = 30
-  records   = [aws_instance.instance[each.value["name"]].private_ip]
-}
+  resource "aws_route53_record" "records" {
+    for_each = var.components
+    zone_id  = "Z01796182PXCN3W0FPGMA"
+    name     = "${each.value["name"]}-test.sreenivasulareddydevops.online"
+    type     = "A"
+    ttl      = 30
+    records  = [aws_instance.instance[each.value["name"]].private_ip]
+  }
