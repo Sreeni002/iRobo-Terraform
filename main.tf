7d9a23a8a0f69c461e5a56ffda5448c6bec3b2a8
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-02 17:25:23 +0530

    Create subnets

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source = "git::https://github.com/Sreeni002/tf-module-vpc.git"
 
   for_each = var.vpc
+  subnets = each.value["subnets"]
   cidr_block = each.value["cidr_block"]
   tags = local.tags
   env = var.env
