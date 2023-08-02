b11b7f8a9b2ea0998985363698553ef03e4a4434
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-02 12:04:00 +0530

    Adding tags

--- var.tf
+++ var.tf
@@ -1,7 +1,3 @@
 variable "env" {}
 variable "vpc" {}
-#variable "database_servers" {}
-#variable "app_servers" {}
-#variable "instance_type" {}
-#variable "component_name" {}
-#variable "password" {}
+variable "tags" {}
