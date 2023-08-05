1b0b4192b4e5f2e4155a8ad9941d49ecb39a711b
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-05 11:12:12 +0530

    Attach ec2 to docdb

--- main.tf
+++ main.tf
@@ -11,39 +11,39 @@ module "vpc" {
   default_vpc_rtid = var.default_vpc_rtid
 }
 
-#module "app" {
-#  source = "git::https://github.com/Sreeni002/tf-module-app.git"
-#
-#  for_each = var.app
-#  instance_type = each.value["instance_type"]
-#  name = each.value["name"]
-#  desired_capacity = each.value["desired_capacity"]
-#  max_size = each.value["max_size"]
-#  min_size = each.value["min_size"]
-#
-#  env = var.env
-#  bastion_cidr = var.bastion_cidr
-#  tags = local.tags
-#
-#  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-#  vpc_id = lookup(lookup(module.vpc, "main", null), "vpd_id", null)
-#
-#  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
-#}
+module "app" {
+  source = "git::https://github.com/Sreeni002/tf-module-app.git"
 
-module "docdb" {
-  source = "git::https://github.com/Sreeni002/tf-module-docdb.git"
+  for_each = var.app
+  instance_type = each.value["instance_type"]
+  name = each.value["name"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size = each.value["max_size"]
+  min_size = each.value["min_size"]
 
-  for_each = var.docdb
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  engine_version = each.value["engine_version"]
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
-
-  tags = local.tags
   env = var.env
-  vpc_id = local.vpc_id
-  kms_arn = var.kms_arn
+  bastion_cidr = var.bastion_cidr
+  tags = local.tags
 
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpd_id", null)
+
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
 }
+
+#module "docdb" {
+#  source = "git::https://github.com/Sreeni002/tf-module-docdb.git"
+#
+#  for_each = var.docdb
+#  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  engine_version = each.value["engine_version"]
+#  instance_count = each.value["instance_count"]
+#  instance_class = each.value["instance_class"]
+#
+#  tags = local.tags
+#  env = var.env
+#  vpc_id = local.vpc_id
+#  kms_arn = var.kms_arn
+
+#}
