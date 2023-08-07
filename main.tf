bf6dee15e235ae90990f877f0a9baf78014d2fc8
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-07 10:10:55 +0530

    Create ealstcache

--- main.tf
+++ main.tf
@@ -62,3 +62,20 @@ module "rds" {
   vpc_id = local.vpc_id
   kms_arn = var.kms_arn
 }
+
+module "elasticache" {
+  source = "git::https://github.com/Sreeni002/tf-module-elasticache.git"
+
+  for_each = var.elasticache
+  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
+  node_type = each.value["node_type"]
+  num_node_groups = each.value["num_node_groups"]
+  replicas_per_node_group = each.value["replicas_per_node_group"]
+
+  tags = local.tags
+  env = var.env
+  vpc_id = local.vpc_id
+  kms_arn = var.kms_arn
+}
